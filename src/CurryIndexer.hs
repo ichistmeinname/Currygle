06da4ec67a3140b944537311f8a58112f41d8480
Sandra Dylus <sad@informatik.uni-kiel.de>
2012-07-10 21:02:59 +0200

    Add functionality to update the existing index with new data (--u) or to create a new index with the given data (--n). Add arguments for curryIndexer (uri path, directory path and options).

--- src/CurryIndexer.hs
+++ src/CurryIndexer.hs
@@ -10,42 +10,179 @@ import CurryInfo
 import IndexTypes
 import Helpers (splitOnWhitespace, biasedWord, typeSignature, consSignature)
 import System.Environment (getArgs)
+import System.Directory (renameFile, getDirectoryContents)
+import System.FilePath.Posix
+import CurrySearch
+
+import Holumbus.Index.CompactSmallDocuments
 
 main :: IO ()
 main = do
-     (a:b:_) <- getArgs
-     main2 a b
-     return ()
+     args <- getArgs
+     processArgs args
 
-main2 :: FilePath -> FilePath -> IO ()
-main2 cdocPath uriP = do
-    putStr $ "Writing index ..."
+processArgs :: [String] -> IO ()
+processArgs args = do
+    case args of 
+         [cdocPath,uriPath,"--n"] -> main2 cdocPath uriPath True
+         [cdocPath,uriPath,"--u"] -> main2 cdocPath uriPath False
+         _                        -> putStr howToUseMessage
+
+howToUseMessage :: String
+howToUseMessage = 
+    "\n"++"No no no, you don't use it right. Let me give you a hint. \n"++
+    "Try it like this: curryIndexer <cdoc_directory> <documentation_uri> [--n|--u]\n" ++
+    "Inwhich '--u' updates your current index with the new data and '--n' creates a new index with the given data.\n"
+
+
+main2 :: FilePath -> FilePath -> Bool -> IO ()
+main2 cdocP uriP new = do
+    putStr $ "Writing index..."
+    files <- getDirectoryContents cdocP >>= return . filter (\c -> takeExtension c == ".cdoc")
+    let uriPath     = fullPath uriP 
+        cdocPath    = fullPath cdocP
+        -- newIndex    = new == "--n" 
+        -- firstFile   = head files
+        directoryFiles = map (cdocPath ++) files
+    writeIndex new uriPath directoryFiles
+    putStr "done!\n"
+  where fullPath path = if last path == '/' then path else path ++ "/"
+
+--  Takes an uriPath and a list of files
+-- | Writes a new index when flag is true, merges new index with existing one otherwise
+writeIndex :: Bool -> FilePath -> [FilePath] -> IO ()
+writeIndex _ _ [] = do 
+           putStr $ "There are no cdoc-Files in this directory" 
+-- writeIndex new uriPath [file] = do
+--     let emptyStates = (emptyCurryModState,emptyCurryFctState,emptyCurryTypeState)
+--     (nM, nF, nT) <- prepareIndex uriPath file (return emptyStates)
+--     -- writeIndex' $ return (nM, nF, nT)
+--     if new then writeIndex' (nM, nF, nT)
+--            else mergeIdxDoc (indexerStates (moduleIndex, functionIndex, typeIndex)) (nM, nF, nT)
+
+writeIndex new uriPath files = do
+   let emptyStates = (emptyCurryModState, emptyCurryFctState, emptyCurryTypeState)
+   (nM,nF,nT) <- foldr (prepareIndex uriPath) (return emptyStates) files
+   if new then writeIndex' (nM,nF,nT) 
+          else mergeIdxDoc (indexerStates (moduleIndex, functionIndex, typeIndex)) (nM, nF, nT)
+
+mergeIdxDoc :: IO ((CompactInverted, SmallDocuments ModuleInfo), 
+                   (CompactInverted, SmallDocuments FunctionInfo), 
+                   (CompactInverted, SmallDocuments TypeInfo)) -> (CurryModIndexerState, CurryFctIndexerState, CurryTypeIndexerState) -> IO ()
+
+mergeIdxDoc ixDoc1 (cMod, cFct, cTyp) = do
+    ((iM1, m1), (iF1, f1), (iT1, t1)) <- ixDoc1
+    let (mDoc, mIndex) = unionDocIndex m1 iM1 (editDoc cMod m1) (editIndex cMod m1)
+    let (fDoc, fIndex) = unionDocIndex f1 iF1 (editDoc cFct f1) (editIndex cFct f1)
+    let (tDoc, tIndex) = unionDocIndex t1 iT1 (editDoc cTyp t1) (editIndex cTyp t1)
+    writeDocIndex moduleIndex mDoc mIndex
+    writeDocIndex functionIndex fDoc fIndex
+    writeDocIndex typeIndex tDoc tIndex
+       
+  where editIndex state state2 = inverted2compactInverted $ updateDocIds' (addDocId (maxId state2)) (ixs_index $ state) 
+        docTable = docTable2smallDocTable . ixs_documents
+        maxId   state = maxKeyDocIdMap $ idToSmallDoc $ state
+        editDoc state state2 = editDocIds (addDocId $ maxId state2) $ docTable state
+        
+writeDocIndex :: (Binary a) => FilePath -> SmallDocuments a -> CompactInverted -> IO ()
+writeDocIndex path cDoc cIndex = do
+    putStr "."
+    writeBin (path++"2.idx") cIndex
+    writeBin (path++"2.doc") cDoc
+    reorganizeFiles path
+
+reorganizeFiles :: FilePath -> IO ()
+reorganizeFiles path = do
+    renameFile (path++"2.idx") (path++".idx") 
+    renameFile (path++"2.doc") (path++".doc")
+
+writeIndex' :: (CurryModIndexerState, CurryFctIndexerState, CurryTypeIndexerState) -> IO ()
+writeIndex' (curryModState, curryFctState, curryTypeState) = do
+    putStr "."
+    writeSearchBin moduleIndex curryModState
+    putStr "."
+    writeSearchBin functionIndex curryFctState
+    putStr "."
+    writeSearchBin typeIndex curryTypeState
+
+    
+indexerStates :: (FilePath, FilePath, FilePath) -> IO ((CompactInverted, SmallDocuments ModuleInfo), 
+                                                       (CompactInverted, SmallDocuments FunctionInfo), 
+                                                       (CompactInverted, SmallDocuments TypeInfo))
+indexerStates (mp, fp, tp) = do 
+       cMod  <- loadModIdxDoc mp
+       cFct  <- loadFctIdxDoc fp
+       cTyp  <- loadTypeIdxDoc tp
+       return (cMod, cFct, cTyp)
+
+-- Helper function for writing index
+prepareIndex :: FilePath -> FilePath -> IO (CurryModIndexerState, 
+                                         CurryFctIndexerState, 
+                                         CurryTypeIndexerState) -> IO (CurryModIndexerState, 
+                                                                          CurryFctIndexerState, 
+                                                                          CurryTypeIndexerState)
+prepareIndex uriPath cdocPath states = do
+    (cMod, cFct, cTyp) <- states
     curryDoc <- loadFromCurryFile $ cdocPath
     let curryModState  = ixDoc 
                          contextsMod 
                          [moduleInfo curryDoc] 
                          [doc uriPath mName (ModuleUri mName) (moduleInfo curryDoc)] 
-                         emptyCurryModState
+                         cMod
         curryFctState  = ixDoc 
                          contextsF 
                          (functionInfos curryDoc) 
                          (map (doc uriPath fName (FctOrTypeUri fModule fName)) $ functionInfos curryDoc)
-                         emptyCurryFctState
+                         cFct
         curryTypeState = ixDoc 
                          contextsT 
                          (typeInfos curryDoc) 
                          (map (doc uriPath tName (FctOrTypeUri tModule tName)) $ typeInfos curryDoc) 
-                         emptyCurryTypeState
-        uriPath = uriP ++ "/"
-    putStr $ " done!\n"
-    writeSearchBin "../index/ix-mod.bin"  $ curryModState
-    writeSearchBin "../index/ix-fct.bin"  $ curryFctState
-    writeSearchBin "../index/ix-type.bin" $ curryTypeState
+                         cTyp
+    putStr "."
+    return (curryModState, curryFctState, curryTypeState)
+
+
+------------------------------------
+
+-- short names for loading file paths
+loadModIdxDoc :: FilePath ->  IO (CompactInverted, SmallDocuments ModuleInfo)
+loadModIdxDoc fp = do
+    index <- decodeFile (fp++".idx") 
+    dcmts <- loadModDocuments (fp++".doc")
+    return (index, dcmts)
+
+loadFctIdxDoc :: FilePath ->  IO (CompactInverted, SmallDocuments FunctionInfo)
+loadFctIdxDoc fp = do
+    index <- decodeFile (fp++".idx") 
+    dcmts <- loadFctDocuments (fp++".doc")
+    return (index, dcmts)
+
+loadTypeIdxDoc :: FilePath ->  IO (CompactInverted, SmallDocuments TypeInfo)
+loadTypeIdxDoc fp = do
+    index <- decodeFile (fp++".idx") 
+    dcmts <- loadTypeDocuments (fp++".doc")
+    return (index, dcmts)
+
+moduleIndex :: FilePath
+moduleIndex = "./index/ix-mod.bin"
+-- moduleIndex2 :: FilePath
+-- moduleIndex2 = "./index/ix-mod.bin2"
+
+functionIndex :: FilePath
+functionIndex = "./index/ix-fct.bin"
+-- functionIndex2 :: FilePath
+-- functionIndex2 = "./index/ix-fct.bin2"
+
+typeIndex :: FilePath
+typeIndex = "./index/ix-type.bin"
+-- typeIndex2 :: FilePath
+-- typeIndex2 = "./index/ix-type.bin2"
+------------------------------------
 
 -- |  Main indexer method to build indexes and documents
 ixDoc :: (Binary a) => (a -> DocId -> [(String, String, Occurrences)]) -> 
-                       [a] -> 
-                       [Document a] -> 
+                       [a] -> [Document a] -> 
                        IndexerState Inverted Documents a -> 
                        IndexerState Inverted Documents a
 ixDoc contextList (info:infos) (doc1:docs) (IndexerState ix dc) = 
