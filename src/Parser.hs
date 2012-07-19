638352ec46ebe975b681aedc44e1c8ce98ef45fe
Sandra Dylus <sad@informatik.uni-kiel.de>
2012-07-19 23:06:37 +0200

    Fix bug: search for monadic types like IO () or Maybe. Add feature: detects signature queries (like "Int->Int").

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,16 +1,16 @@
 module Parser where
 
-import           Holumbus.Query.Language.Grammar
-import           Holumbus.Query.Language.Parser
 import           Data.List
 import           Data.Char
 import           Data.Maybe
--- import           Data.Map as Map
+
+import           Holumbus.Query.Language.Grammar
+import           Holumbus.Query.Language.Parser
+
 import           Helpers
 
--- import Debug.Trace (trace)
+import Debug.Trace (trace)
 
--- import Debug.Trace (trace)
 
 prepareQuery :: String -> Either String Query
 prepareQuery s
@@ -23,18 +23,60 @@ specify names "" = BinQuery Or (Specifier names $ Word "") (Specifier names (Neg
 specify names str = Specifier names $ Word str
 
 customQuery :: String -> Either String Query
-customQuery s = Right $ customize $ splitOnWhitespace s
-  where customize :: [String] -> Query
-        customize [x] = if binary x then Word "" else custom x
-        customize (x:y:xs) 
-          | x == "NOT" = BinQuery But (Word "") (customize $ y:xs)
-          | binary x = customize $ y:xs 
-          | not (isSpecifier y || binary y) = customize $ (x++y):xs
-          | binary y = binQuery y (custom x) $ customize xs
-          | isSpecifier y = BinQuery And (custom x) (customize $ y:xs)
-        customize _ = Word ""
-        binary x = x `elem`["AND", "OR", "NOT"]
-        
+customQuery s 
+  | isSignature s = trace (show $ customize $ searchForSignature $ splitQuery s) (Right $ customize $ searchForSignature $ splitQuery s)
+  | otherwise     = trace (show $ customize $ splitQuery s) (Right $ customize $ splitQuery s)
+ where splitQuery = searchForParens . searchForMonads . fixForSignatures
+       fixForSignatures = intercalate ["->"] . removeEmptyStrings . map splitOnWhitespace . splitOnArrow
+       removeEmptyStrings = map (filter (\x -> not $ x == []))
+
+customize :: [String] -> Query
+customize [x] = if binary x then Word "" else custom x
+customize (x:y:xs) 
+  | x == "NOT" = BinQuery But (Word "") (customize $ y:xs)
+  -- | isSignature x = customize $ (":signature"++x):y:xs
+  | binary x = customize $ y:xs 
+  | "(" `isPrefixOf` y && ")" `isSuffixOf` y = customize $ (x ++ (strip "(" $ init y)) : xs
+  | not (isSpecifier y || binary y) = customize $ (x++y) : xs
+  | binary y = binQuery y (custom x) $ customize xs
+  | isSpecifier y = BinQuery And (custom x) (customize $ y:xs)
+customize (x:xs) 
+  | isSignature x = custom $ ":signature"++x
+  | otherwise = Word $ intercalate " " $ x:xs
+customize [] = Word ""
+
+isMonad :: String -> Bool
+isMonad str = 
+  any (flip isInfixOf $ string str) ["maybe", "io", "ioref"]
+ where string = last . splitOnWhitespace . map toLower
+
+searchForMonads :: [String] -> [String]
+searchForMonads (x:y:xs) = 
+ if isMonad x && not (isSpecifier y || binary y) then [x++" "++y] ++ searchForMonads xs
+                                                 else x : searchForMonads (y:xs)
+searchForMonads xs = xs
+
+searchForParens :: [String] -> [String]
+searchForParens (x:xs) = 
+  if "(" `isPrefixOf` x then endParen (x, xs) else x : searchForParens xs
+ where endParen (parenString, (y:ys)) = 
+         if ")" `isSuffixOf` y then (parenString ++ y) : ys
+                               else endParen (parenString++y, ys)
+       endParen (p, []) = [p]
+searchForParens [] = [[]]
+
+searchForSignature :: [String] -> [String]
+searchForSignature (x:y:xs) = 
+  if y == "->" then endSignature ((x++y), xs) else x : searchForSignature (y:xs)
+ where endSignature (sig, (sigComp:str:rest)) = 
+         if not $ str == "->" then (sig ++ sigComp):str:rest 
+                            else endSignature (sig++sigComp++str,rest)
+       endSignature (sig, [end]) = [sig++end]
+       endSignature (sig, []) = [sig]
+searchForSignature xs = xs
+
+binary :: String -> Bool        
+binary = flip elem ["AND", "OR", "NOT"]
 
 customized :: String -> Bool
 customized s 
@@ -47,7 +89,7 @@ customized s
 custom :: String -> Query
 custom x
     | funcSpecifier x  = specify ["Function"] (strip ":function" x)
-    | typeSpecifier x  = specify ["Type"] (strip ":typ" x) 
+    | typeSpecifier x  = specify ["Type"] (strip ":type" x) 
     | modSpecifier x   = specify ["Module"] (strip ":module" x)
     | signature x      = specify ["Signature"] (strip ":signature" x)
     | ndSpecifier x    = specify ["NonDet"] (strip ":nondet" x)
@@ -71,14 +113,6 @@ isSpecifier s = any (`isPrefixOf` s) specifiers
 caseSensitive :: String -> Bool
 caseSensitive [] = False
 caseSensitive (x:_) = isUpper x
-
--- normalizeSignature :: String -> String
--- -- (Int -> Char)-> [Int] -> [Char]
--- normalizeSignature s = trace (show (Prelude.map (\c -> (pairMap s) ! [c]) s)) (Prelude.map (\c -> (pairMap s) ! [c]) s) 
---   where pairMap = insertNext (Map.fromList []) (ord 'a') . splitOnArrow
---         insertNext pMap _ [] = pMap
---         insertNext pMap i (x:xs) = if Map.notMember x pMap then insertNext (Map.insert x (chr i) pMap) (i+1) xs else insertNext pMap i xs
-
 
 -- list of all definied specifiers
 specifiers :: [String] 
@@ -118,7 +152,7 @@ signature s = isSignature s || sigSpecifier s
 
 strip :: String -> String -> String
 strip prefix s = 
-    if isPrefixOf prefix str then fromJust $ stripPrefix prefix str
+    if isPrefixOf prefix str then fromJust $ stripPrefix prefix s
                              else s
   where str = concat $ splitOnWhitespace s
 
