f2ffad16484fb253e0ca3fef2480c97c3306d157
Sandra Dylus <sad@informatik.uni-kiel.de>
2012-08-14 18:42:48 +0200

    Fix bug: auto-completion selects first suggestion entry by default.

--- resources/static/searching.js
+++ resources/static/searching.js
@@ -14,30 +14,32 @@ function createRequestObject() {
 var http=createRequestObject();
 
 function searchQuery() { 
-  location.href="/currygle?query="+encodeURIComponent(document.getElementById("query").value);
-    return false;
+  location.href="/currygle?query="
+   +encodeURIComponent(document.getElementById("query").value);
+  return false;
 }
 
 function getCompletions(e) {
-   var query=encodeURIComponent(document.getElementById("query").value);
-   var autocomplete=$('input').typeahead();
-   switch(e.keyCode) {
-        case 9: // tab
-        case 13: // enter
-       return;
+  var query=encodeURIComponent(document.getElementById("query").value);
+  var autocomplete=$('input').typeahead();
+  switch(e.keyCode) {
+    case 9: // tab
+    case 13: // enter
+
+    return;
      
-        case 27: // escape
-          //autocomplete.data('typeahead').hide();
-          return;
-        case 37: // arrows
-        case 38:
-        case 39:
-        case 40:    
-          e.preventDefault()
-          //autocomplete.data('typeahead').keyup(e)
-          return;
+    case 27: // escape
+        //autocomplete.data('typeahead').hide();
+    return;
+    case 37: // arrows
+    case 38:
+    case 39:
+    case 40:    
+    e.preventDefault()
+        //autocomplete.data('typeahead').keyup(e)
+    return;
    }
-    var lastBlankPos = query.lastIndexOf(' ');
+  var lastBlankPos = query.lastIndexOf(' ');
   if (query.length>lastBlankPos+1) {
       query=query.substring(lastBlankPos);
     }
